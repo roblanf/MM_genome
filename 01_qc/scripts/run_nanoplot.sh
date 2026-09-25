57a931cee9ef4c9add082a79e329d7dadf6fe7cf
Jack Davies <jack@rona.rsb.anu.edu.au>
2026-09-25 11:12:06 +1000

    Update NanoPlot script under new config scheme

--- 01_qc/scripts/run_nanoplot.sh
+++ 01_qc/scripts/run_nanoplot.sh
@@ -1,22 +1,21 @@
 #!/usr/bin/env bash
 # Script: 01_qc/scripts/run_nanoplot.sh
 # Purpose: Runs NanoPlot on raw ONT FASTQ reads (pre-filtering baseline).
-# Usage: bash 01_qc/scripts/run_nanoplot.sh [input_fastq] [output_dir] [threads]
 
-#!/usr/bin/env bash
 set -euo pipefail
 
-INPUT_FASTQ="${1:-raw_data}"
-OUTDIR="${2:-01_qc/results/nanoplot_test_output}"
-THREADS="${3:-16}"
+# loads config file to use raw or test data depending on user choice
+source config.sh
 
-mkdir -p "${OUTDIR}"
+nanoplot_dir="01_qc/results/nanoplot"
 
-NanoPlot \
-  --threads "${THREADS}" \
-  --fastq "${INPUT_FASTQ}"/*fastq* \
-  --outdir "${OUTDIR}" \
-  --loglength \
-  --N50 \
-  --title "E. phylacis Raw ONT QC" \
-  --tsv_stats
+mkdir -p "${nanoplot_dir}"
+
+# Run NanoPlot
+NanoPlot -t 128 \
+         --fastq "${raw_data}"/*.fastq.gz \
+         --downsample 100000 \
+         -o "${nanoplot_dir}" \
+         --title "E. phylacis ONT reads NanoPlot"
+#Uses 128 threads, change at your discretion depending on server capacity
+# downsample 100000 means randomly sample max of 100K reads to generate nanoplot and summary stats, avoid computation overload.
