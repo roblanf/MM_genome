f84c07c4d3858996d62e616a5887300359061b19
Jack Davies <jack@rona.rsb.anu.edu.au>
2026-09-25 10:17:59 +1000

    Add configuration script to easily switch between testing and real data

---
+++ config.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+set -euo pipefail
+#!/bin/bash
+
+# Configure between using test data or real data for running the scripts!
+# To switch between test/real data, remove the # symbol from what you want to use and add # to the other line.
+# Happy coding!
+
+# 1. TEST DATA
+raw_data="test_data"
+
+# 2. REAL DATA
+# raw_data="/home/raw_data/MM/2025_long_reads/ONT_gDNA192_1071_RL/E_Phylacis/20251117_1227_2C_PBE85256_e040940c/fastq_pass"
