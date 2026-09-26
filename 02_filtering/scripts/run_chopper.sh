61f4b85535e070849f58090b82c67db4c52acd8f
Jack Davies <jack@rona.rsb.anu.edu.au>
2026-09-27 01:10:09 +1000

    Update Filtering to >15Kb instead of >17.5kb for more coverage

--- 02_filtering/scripts/run_chopper.sh
+++ 02_filtering/scripts/run_chopper.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 # Script: 02_filtering/scripts/run_chopper.sh
-# Purpose: Decompress files with pigz, filter reads using >=Q15 and length >=17.5kb using chopper.
+# Purpose: Decompress files with pigz, filter reads using >=Q15 and length >=15kb using chopper.
 # These cutoffs are set to provide good quality whilst maintaining >30x coverage per haplotype.
 
 set -euo pipefail
@@ -15,5 +15,5 @@ out_fastq="${filter_dir}/filtered_reads.fastq.gz"
 # Use piping to decompress with pigz, then filter with chopper, and compress again
 find ${raw_data} -type f \( -name "*.fastq.gz" -o -name "*.fq.gz" \) | \
   xargs pigz -dc -p 64 | \
-  chopper -q 15 -l 17500 | \
+  chopper -q 15 -l 15000 | \
   pigz -p 64 > "${out_fastq}"
