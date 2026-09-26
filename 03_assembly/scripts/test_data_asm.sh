99999e97808ae0fcbc80932a11486cd7d35bea40
Jack Davies <jack@rona.rsb.anu.edu.au>
2026-09-27 00:41:52 +1000

    Fix naming issue in filtered test data script

--- 03_assembly/scripts/test_data_asm.sh
+++ 03_assembly/scripts/test_data_asm.sh
@@ -4,4 +4,4 @@
 #          In config.sh, see the For Hifiasm section and toggle between this test data and real data.
 #          You have to use this instead of test_data because the Hifiasm script needs filtered_fastq not raw_data to run.
 
-zcat 02_filtering/filtered_reads.fastq.gz | head -n 40000 | gzip > 02_filtering/test_filtered.fastq.gz
+zcat 02_filtering/filtered_reads.fastq.gz | head -n 40000 | gzip > 02_filtering/test_filtered_reads.fastq.gz
